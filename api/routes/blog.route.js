cac82f1cc5e99951c830ffac2b1bb5598dd05419
Faruq <olowonlafaruq@gmail.com>
2025-02-19 16:44:20 -0800

    changes

--- api/routes/blog.route.js
+++ api/routes/blog.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createBlog, deleteBlog, editBlog, getAllJobs, getBlogsByCreator, getJobId } from '../controller/blog.controller.js';
+import { createBlog, deleteBlog, editBlog, getAllJobs, getBlogById, getBlogsByCreator } from '../controller/blog.controller.js';
 
 
 const router = express.Router()
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.post('/create',createBlog)
 router.get('/blogs',getAllJobs)
-router.get('/blogs/:id',getJobId)
+router.get('/blogs/:id',getBlogById)
 router.get('/fetch',getBlogsByCreator)
 
 router.patch('/edit/:id',editBlog)
